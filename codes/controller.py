10ed72dbe591854ee163e88979c9dcfe604de290
JaininV <jaininvekariya@gmail.com>
2025-06-24 14:52:10 -0400

    change

--- codes/controller.py
+++
@@ -1,9 +0,0 @@
-#Import users function from model folder
-from flask import Flask, render_template, jsonify, request
-from video_alag import split_video_with_labels
-import asyncio
-
-def videoSplitApi():
-    data = request.form
-    page = split_video_with_labels(data)
-    return page
--- codes/route.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint,  jsonify
-from controller import videoSplitApi
-
-video_split = Blueprint('video_split', __name__)
-@video_split.route('/split_video', methods=['GET'])
-def video_split_api():
-    page = videoSplitApi()
-    return page
